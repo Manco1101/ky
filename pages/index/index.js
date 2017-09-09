f4e9ebb92a2b5e7170458b36cf55e24f3da9c12d
manco1101 <2286881762@qq.com>
2017-09-09 17:21:18 +0800

    pages

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,31 +1,60 @@
 Page({
   data: {
+    motto: 'Hello World',
+    userInfo: '',
     items: [
-      "http://git.oschina.net/manco1101/kunyuan/blob/image/1.jpg",
-      "../../images/2.JPG",
-      "../../images/3.JPG",
-      "../../images/4.JPG",
-      "../../images/5.JPG",    "http://git.oschina.net/moyuan/mumu/blob/master/images/case-1.JPG"
+"https://raw.githubusercontent.com/Manco1101/ky/master/images/1.jpg",      "https://raw.githubusercontent.com/Manco1101/ky/master/images/2.jpg",      "https://raw.githubusercontent.com/Manco1101/ky/master/images/3.jpg",      "https://raw.githubusercontent.com/Manco1101/ky/master/images/4.jpg", "https://raw.githubusercontent.com/Manco1101/ky/master/images/5.jpg",    "https://raw.githubusercontent.com/Manco1101/ky/master/images/6.jpg",
     ],
-    logo: "http://git.oschina.net/moyuan/mumu/raw/master/images/case-9.JPG",
+    logo: "https://raw.githubusercontent.com/Manco1101/ky/master/images/7.jpg",
     phone: '18820188210',
     baoye: {
-      latitude: 31.026569, //纬度
-      longitude: 121.73533299999997 //经度
+      latitude: 22.672531, //纬度
+      longitude: 114.042125 ,//经度
+      name:" 坤元驾校",
     }
+ 
   },
   onCall: function() {
     wx.makePhoneCall({
       phoneNumber: this.data.phone
     })
   },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../feedback/index'
+    })
+  
+  },
+
+  //扫一扫
+  scanCode: function () {
+    wx.scanCode({
+      success: (res) => {
+
+        this.setData({
+          userInfo: res.result
+        })
+      }
+    })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '自定义分享标题',
+      desc: '自定义分享描述',
+      path: '/page/user?id=123'
+    }
+  },
   onLocation: function() {
     wx.openLocation({
       latitude: this.data.baoye.latitude,
       longitude: this.data.baoye.longitude,
+      name:this.data.baoye.name,
+      address:" 广东省深圳市龙华区清泉富景大夏4楼410",
       scale: 28
     })
   },
+
   previewItem: function(e){
     var src = e.currentTarget.dataset.src
     wx.previewImage({
@@ -33,11 +62,18 @@ Page({
       urls: this.data.items
     })
   },
+
   onLoad: function() {
     var that = this
     wx.setNavigationBarTitle({
-      title: "坤元驾校"
+      title: " "
     })
+    
+    wx.showNavigationBarLoading()
+    setTimeout(function () {
+      wx.hideNavigationBarLoading()
+    }, 3000)
+
     wx.getSystemInfo({
       success: function(res){
         var windowWidth = res.windowWidth
@@ -51,5 +87,41 @@ Page({
         })
       }
     })
+  },
+  onReady: function () {
+    // 生命周期函数--监听页面初次渲染完成
+
+    
+  },
+  onShow: function () {
+    // 生命周期函数--监听页面显示
+
+  },
+  onHide: function () {
+    // 生命周期函数--监听页面隐藏
+
+
+  },
+  onUnload: function () {
+    // 生命周期函数--监听页面卸载
+
+  },
+  onPullDownRefresh: function () {
+    // 页面相关事件处理函数--监听用户下拉动作
+
+  },
+  onReachBottom: function () {
+    // 页面上拉触底事件的处理函数
+
+  },
+  onShareAppMessage: function () {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
   }
+
+
 })
