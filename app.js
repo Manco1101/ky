39e0ce8a56625391e97dc2dd9ac1ae7b556c003b
manco1101 <2286881762@qq.com>
2017-09-09 17:25:22 +0800

    app

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 //app.js
+var hotapp = require('utils/hotapp.js');
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    hotapp.init('hotapp246715652');
+    hotapp.setDebug(true);
+  },
+
+  onError: function (msg) {
+    //错误日志上传(开发中)
+    hotapp.onError(msg, '1.0.0', function (err) {
+      console.log(err)
+    })
   },
   getUserInfo:function(cb){
     var that = this
@@ -25,6 +35,7 @@ App({
     }
   },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    hotapp: hotapp // 这里作为一个全局变量, 方便其它页面调用
   }
 })
